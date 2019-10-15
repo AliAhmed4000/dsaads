27d7a610f7b09034294abe78cbfe5fb387e0d4c0
umar <umer@zenofruby.com>
2019-10-15 17:41:46 +0500

    set seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,11 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+user = AdminUser.find_by_email('admin@example.com')
+banner = user.build_banner(:image => Rails.root.join("app/assets/images/outlinesiteagain.docx.jpg").open,title: "banner",description: "This is my Banner")
+banner.save
+titles = ["Cars & Truck", "Classic Cars", "Boat & WaterCraft", "Vehicle Parts, Tries & Accessories", "MotorCycles", "Heavy Equiments"]
+images = ["app/assets/images/classic_cars.jpeg","app/assets/images/classic_cars.jpeg","app/assets/images/classic_cars.jpeg","app/assets/images/classic_cars.jpeg","app/assets/images/classic_cars.jpeg","app/assets/images/classic_cars.jpeg"]
+(0..5).to_a.each do |key|
+  Category.create(:title => titles[key],:image => Rails.root.join(images[key]).open)
+end 
