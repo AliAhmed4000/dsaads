23b3cc1cbbb450ffc61d8c37c43c5527d5b81442
umar <umer@zenofruby.com>
2019-10-16 19:34:32 +0500

    set fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
 
-  config.action_mailer.default_url_options = { :host => ENV['HOST_NAME'], :protocol => 'http' }
+  config.action_mailer.default_url_options = { :host => ENV['HOST_NAME'], :protocol => 'https' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.smtp_settings = {
