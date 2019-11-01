053298168231ebf5941dafcb58290efdd5db0270
umar <umer@zenofruby.com>
2019-11-01 11:17:03 +0500

    fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   def index
   	@categories = Category.get_categories
-    @services = Service.order(created_at: :desc).take(6)
+    @services = Service.where('publish=?',true).order(created_at: :desc).take(6)
   end
 end
