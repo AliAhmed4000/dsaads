945c57be91efe7d9f310fc73222f7a20f272e612
umar <umer@zenofruby.com>
2019-11-05 14:55:19 +0500

    filter

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,6 +24,7 @@
 //= require_tree ./channels
 //= require script
 //= require dropzone
+//= require jquery.form
 
 
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < ActionController::Base
     @category = Category.find_by_id(id)
     unless @category.blank?
       unless params[:q].blank?
-        @q = Service.joins(:category).where('categories.sub_category_id=? and service.publish=?',@category.id,true).ransack(params[:q])
+        @q = Service.joins(:category).where('categories.sub_category_id=? and services.publish=?',@category.id,true).ransack(params[:q])
         @packages_delivery_time = params[:q][:packages_delivery_time_eq]
         @packages_price_gteq = params[:q][:packages_price_gteq]
         @packages_price_lteq = params[:q][:packages_price_lteq]
