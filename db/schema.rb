4947b7130763198063233db1cdd12aa4d5076bd6
Muhammad Umer <umer@zenofruby.com>
2019-11-15 16:27:38 +0500

    adding checkbox to publish

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_08_111947) do
+ActiveRecord::Schema.define(version: 2019_11_14_083826) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -167,6 +167,7 @@ ActiveRecord::Schema.define(version: 2019_11_08_111947) do
     t.boolean "is_commercial"
     t.integer "revision_number"
     t.integer "delivery_time"
+    t.boolean "publish"
     t.index ["service_id"], name: "index_packages_on_service_id"
   end
 
