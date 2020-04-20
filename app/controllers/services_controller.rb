62e605b04a98e520db8017ea8a45142fb3e084b4
umar <umer@zenofruby.com>
2020-04-20 16:15:27 +0500

    fixes

--- app/controllers/services_controller.rb
+++ app/controllers/services_controller.rb
@@ -1,6 +1,6 @@
 class ServicesController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show,:search]
-  before_action :check_seller_profile, except: [:index,:show,:search]
+  before_action :check_seller_profile, except: [:index,:show,:search,:custom_offer,:custom_offer_create,:custom_offer_update,:show_custom_details,:remove_custom_offer]
   before_action :convert_to_seller, only: [:new,:manage_services] 
   before_action :check_servie_owner, only: [:edit,:pricing,:description,:requirement,:gallery,:publish,:update]
   include ActionView::Helpers::UrlHelper
